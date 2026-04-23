4e9034572eed66f67ad8fdf272547f7a62f70dd1
Ahmed Khan <146993459+iahmedd-k@users.noreply.github.com>
2026-04-23 07:57:16 +0500

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,26 @@
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        used = [False] * len(nums)
+
+        def backtrack(current):
+            if len(current) == len(nums):
+                result.append(list(current))
+                return
+
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+
+                used[i] = True
+                current.append(nums[i])
+
+                backtrack(current)
+
+                current.pop()
+                used[i] = False
+
+        backtrack([])
+        return result
